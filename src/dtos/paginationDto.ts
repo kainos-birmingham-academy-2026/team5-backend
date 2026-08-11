791b0b7c3d23a10ca8f86e7bb027304ab8ca00d3
Eyad Selmy <eyadselmy1@gmail.com>
2026-08-11 11:21:45 +0100

    Update paginationDto.ts

--- src/dtos/paginationDto.ts
+++ src/dtos/paginationDto.ts
@@ -3,7 +3,7 @@ import type { JobRoleResponseDto } from "./jobRoleDto";
 
 export const PaginationQuerySchema = z.object({
 	page: z.coerce.number().int().positive().default(1),
-	pageSize: z.coerce.number().int().positive().max(100).default(10),
+	pageSize: z.coerce.number().int().positive().max(10).default(10),
 });
 
 export type PaginationQueryDto = z.infer<typeof PaginationQuerySchema>;
