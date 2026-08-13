6bf91c3a48021ed9a12a60ecbe58077a6447fdf9
Jaleel Shahin <jaleel.shahin@kainos.com>
2026-08-13 11:55:28 +0100

    special character increase

--- src/dtos/userDto.ts
+++ src/dtos/userDto.ts
@@ -18,7 +18,7 @@ export const RegisterRequestSchema = z.object({
 		.min(8, "Password must be at least 8 characters")
 		.regex(/[A-Z]/, "Password must contain at least 1 uppercase letter")
 		.regex(/[a-z]/, "Password must contain at least 1 lowercase letter")
-		.regex(/[!@#$%^&*]/, "Password must contain at least 1 special character (!@#$%^&*)"),
+		.regex(/[!@#$%^&*?.\-_,;:'"\[\]{}()<>+=/\\]/, "Password must contain at least 1 special character (!@#$%^&*?.-_,;:'\"[]{}()<>+=/\\)"),
 });
 
 export type RegisterRequestDto = z.infer<typeof RegisterRequestSchema>;
